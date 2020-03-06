a9cbd586da311f705e1402af1ad3a8c01a173e86
Student <example@example.com>
2020-03-05 20:42:10 -0800

    brake app, try to make it work but epically fail. More to come soon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,52 @@
-
-
+function diff(word) {
+  var result = function(inputWord) {
+    
+    var n = inputWord.search(/[aeiuo]/);
+    alert(n);
+    if (n === 0) {
+      return inputWord + "way";
+    } else {
+      return inputWord + "ay";
+    }
+  }
+}
 
 
 $(document).ready(function() {
  $("form#pigLatin").submit(function(event) {
    event.preventDefault();
    var inputWord = $("input#word").val();
-   console.log(inputWord);
+   var result = diff(inputWord);
+   console.log(result);
    console.log(inputWord.charAt(0));
 
-   var newWord = [];
-   if (inputWord.charAt(0) === "a") {
-     alert(inputWord);
-   } 
+
+
+
+
+
+
+
+
+
+
+   //var result = translate(inputWord);
+   //console.log(result(inputWord));
+  //  if (inputWord.charAt(0) === "a") {
+
+  //    alert(inputWord.charAt(1) + "way");
+     //alert(inputWord);
+   //} 
+
+
+
+
+
+
+
+
+
+
 
  });
 });
